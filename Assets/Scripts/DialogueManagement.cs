4cf96c4ea9876a3cb7349b877d1d28b998b184ad
Nina Demirjian <nsd286@nyu.edu>
2018-09-01 15:08:44 -0400

    ui

--- Assets/Scripts/DialogueManagement.cs
+++ Assets/Scripts/DialogueManagement.cs
@@ -7,8 +7,12 @@ public class DialogueManagement : MonoBehaviour {
 
     public string text;
     public Canvas speech;
+    public Canvas spaceIndication; //this is the little popup to tell the user to press space
 
-    bool restartCoroutine = true; 
+    bool talkingEnabled = false;
+    bool dialogueUp = false;
+    bool restartCoroutine = true;  //used for whether or not to start the coroutine of putting tect on scfreen 
+    //when player moves by another toy
 
     Transform child;
     Text contents;
@@ -18,42 +22,63 @@ public class DialogueManagement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         speech.enabled = false;
+        spaceIndication.enabled = false;
         child = speech.transform.Find("Text");
         contents = child.GetComponent<Text>();
         coroutine = delayedSpeech(contents);
-
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    void Update()
     {
-        speech.enabled = true; //show the speech bubble
-        if (restartCoroutine)
-        {
-            StartCoroutine(coroutine);
-            restartCoroutine = false;
-        }
-        else{
-            contents.text = text;
+        if(talkingEnabled){
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (dialogueUp == false)
+                {
+                    spaceIndication.enabled = false;
+                    dialogueUp = true;
+                    speech.enabled = true; //show the speech bubble
+                    if (restartCoroutine) //if the coroutine hasn't run yet
+                    {
+                        StartCoroutine(coroutine);
+                        restartCoroutine = false;
+                    }
+                    else
+                    {
+                        contents.text = text;
+                    }
+                }
+                else{
+                    spaceIndication.enabled = true;
+                    dialogueUp = false;
+                    speech.enabled = false; //turn off the spech bubble
+                    StopCoroutine(coroutine); //stop the typing!
+                    contents.text = text;
+                }
+            }
         }
     }
-    void OnCollisionExit2D(Collision2D col)
-    {
-        speech.enabled = false; //turn off the spech bubble
-        StopCoroutine(coroutine);
-        contents.text = text;
 
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        talkingEnabled = true;
+        spaceIndication.enabled = true;
+
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        talkingEnabled = false;
+        speech.enabled = false;
+        spaceIndication.enabled = false;
     }
 
     IEnumerator delayedSpeech(Text contents){ //make the text appear letter by letter
         char[] c = text.ToCharArray();
         for (int i = 0; i < text.Length; i++)
         {
-            yield return new WaitForSeconds(.03f); //time inbetween characters appearing on screen
+            yield return new WaitForSeconds(.05f); //time inbetween characters appearing on screen
             contents.text = contents.text + c[i];
         }
         contents.text = text;
-
     }
-
-
 }
---
+++ Assets/Scripts/LevelTitleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTitleScript : MonoBehaviour {
+    public Text text;
+    public Image image;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(fading());
+    }
+
+    IEnumerator fading(){
+        yield return new WaitForSeconds(1f);
+        fade();
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+    void fade(){
+        Color colorToFadeTo = new Color(1f, 1f, 1f, 0f);
+        image.CrossFadeColor(colorToFadeTo, 1f, true, true);
+        text.CrossFadeColor(colorToFadeTo, 1f, true, true);
+    }
+
+}
