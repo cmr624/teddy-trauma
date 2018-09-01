9bb63b763bdd6193d3444ac2792448ed2bbae958
Nina Demirjian <nsd286@nyu.edu>
2018-09-01 18:13:41 -0400

    even more ui

---
+++ Assets/Scripts/UI/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(fading());
+    }
+
+    IEnumerator fading()
+    {
+        yield return new WaitForSeconds(4f);
+  
+        Destroy(gameObject);
+    }
+
+}
