0f7a80a8d31f44a3f280a0e2b5bb1f68799b45cc
Carlos Michael <cmr624@nyu.edu>
2018-08-31 23:36:06 -0400

    added movement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,13 +4,89 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 CurrentVelocity;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            //Debug.Log("up");
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                //Debug.Log("up/right");
+                rb.velocity = new Vector2(speed, speed);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                //Debug.Log("up/left");
+                rb.velocity = new Vector2(-speed, speed);
+            }
+            else if (Input.GetAxis("Vertical") > 0)
+            {
+                rb.velocity = new Vector2(0, speed);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, 0);
+            }
+        }
+        else if (Input.GetKeyUp("up"))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                rb.velocity = new Vector2(speed, -speed);
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                rb.velocity = new Vector2(-speed, -speed);
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                rb.velocity = new Vector2(0, -speed);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, 0);
+            }
+        }
+        else if (Input.GetKeyUp("down"))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+        else if (Input.GetAxis("Horizontal") == 0)
+        {
+            rb.velocity = new Vector2(0f, 0f);
+        }
+        else if (Input.GetAxis("Vertical") == 0)
+        {
+            rb.velocity = new Vector2(0f, 0f);
+        }
+        if (!Input.anyKey)
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+        //rb.velocity = new Vector2(CurrentVelocity.x, CurrentVelocity.y);
+
+    }
 }
