0640f16b9427c4a840d814d4ffd98744b3152c0a
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-09-02 14:39:29 -0400

    added animator script

--- Assets/Scripts/AnimationPlayer.cs
+++ Assets/Scripts/AnimationPlayer.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationPlayer : MonoBehaviour {
+public class AnimationPlayer : MonoBehaviour
+{
 
+	private Animator animator;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			animator.SetTrigger("left");
+		}
+		else if (Input.GetAxis("Horizontal") > 0)
+		{
+			animator.SetTrigger("right");
+		}
+		else if (Input.GetAxis("Vertical") < 0)
+		{
+			animator.SetTrigger("down");
+		}
+		else if (Input.GetAxis("Vertical") > 0)
+		{
+			animator.SetTrigger("up");
+		}
+		if (!Input.anyKey)
+		{
+			animator.SetTrigger("idle");
+		}
 	}
 }
