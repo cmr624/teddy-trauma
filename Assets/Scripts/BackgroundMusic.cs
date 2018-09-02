4e5cf97ec7aecef397233166ca560e98b4713fd2
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-09-02 14:33:33 -0400

    arty

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+
+	public AudioClip Music;
+	public AudioSource MusicSource;
+
+	// Use this for initialization
+	void Start()
+	{
+		MusicSource.clip = Music;
+		MusicSource.Play();
+	}
+}
